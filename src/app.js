89d1e59fc92bd91f822352ec37f9393cc81312be
Javier Portillo <javiermarquezportillo@gmail.com>
2017-03-23 02:23:53 -0600

    Add modules to test babel transpiling of ES6 features

---
+++ src/app.js
@@ -0,0 +1,31 @@
+// TESTING SOME ES6 FEATURES WITH BABEL
+
+let a = 1000;
+
+if (true === true) {
+    let a = 1;
+    console.log(a); // 1
+}
+
+console.log(a); // 1000
+
+function rest(a, b, ...c) {
+    console.log(c) // [3, 4, 5]
+
+    return a + b;
+}
+
+rest(1, 2, 3, 4, 5);
+
+const doNotMutateMePlease = {
+    name: 'Jane',
+    age: 20
+}
+
+const imTrying = {
+    ...doNotMutateMePlease,
+    age: 35
+}
+
+console.log(doNotMutateMePlease.age); // 20
+console.log(imTrying.age); // 35
