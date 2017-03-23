460a70677b94854767b085b619fe9056f418f00f
Javier Portillo <javiermarquezportillo@gmail.com>
2017-03-23 02:22:42 -0600

    Sets up babel environment

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,21 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'stage-3'],
+                        plugins: ['transform-runtime']
+                    }
+                }
+            }
+        ]
     }
 }
