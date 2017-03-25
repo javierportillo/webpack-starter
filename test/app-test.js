3f915164c6dc6d229754c4f97f1a1a6f2688ebd8
Javier Portillo <javiermarquezportillo@gmail.com>
2017-03-25 01:10:23 -0600

    Add app test source

---
+++ test/app-test.js
@@ -0,0 +1,15 @@
+import chai from 'chai';
+
+const expect = chai.expect;
+
+describe('Mocha', function () {
+    it('should run this test successfully', function () {
+
+    });
+});
+
+describe('Chai', function () {
+    it('should assert tests successfully', function () {
+        expect(true).to.be.true;
+    });
+});
